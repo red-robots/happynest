f7d2dfd339362cb401edc23bfa30f612f3ce5df7
lisadebona <lisaqdebona@gmail.com>
2023-05-25 00:10:38 -0400

    styled sitemap and 404 page

--- page-sitemap.php
+++ page-sitemap.php
@@ -1,21 +1,23 @@
 <?php
+/**
+ * Template Name: Sitemap
+ */
+
 $bannerImage = get_field('banner_image');
 $hasBanner = ( isset($bannerImage['image1']) && $bannerImage['image1'] ) ? 'hasBanner' : 'noBanner';
 get_header(); 
+
+print_r($banner);
 ?>
 
-<div id="primary" class="content-area-full internalPage generic-layout <?php echo $hasBanner ?>">
-  <main id="main-content" class="main-content">
-  <?php while ( have_posts() ) : the_post(); ?>
+<div id="primary" class="content-area-full sitemap generic-layout <?php echo $hasBanner ?>">
+  <main id="main" class="site-main wrapper" role="main">
+    <?php while ( have_posts() ) : the_post(); ?>
 
       <?php if ($hasBanner=="noBanner") { ?>
-      <div class="titlediv typical">
+      <div class="titlediv">
         <h1 class="page-title"><span><?php the_title(); ?></span></h1>
       </div>
-      <?php } ?>
-
-      <?php if ( get_the_content() ) { ?>
-      <div class="entry-content"><?php the_content(); ?></div>
       <?php } ?>
 
       <?php  
@@ -32,13 +34,17 @@ get_header();
             <div class="intro-text"><?php echo anti_email_spam($intro_text) ?></div>
           </div>
         </section>
-      <?php } ?>  
-    
-  <?php endwhile; ?>
-  </main>
-</div><!-- #primary -->
+      <?php } ?>
 
-<?php get_template_part('parts/steps'); ?>
+      <?php if ( has_nav_menu('sitemap') ) { ?>
+      <div id="sitemap-wrap">
+        <?php wp_nav_menu( array( 'theme_location' => 'sitemap', 'menu_id' => 'sitemap','container_class'=>'sitemap-links') ); ?>
+      </div>
+      <?php } ?>
+
+    <?php endwhile; ?>  
+  </main>
+</div>
 
 <?php
 get_footer();
